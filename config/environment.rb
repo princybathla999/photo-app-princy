600acd40fcdaa88ea8d64b6e8d6217c9ceee1ca9
Alpha Blog <princybathla999@yahoo.com>
2017-05-03 12:11:01 -0400

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -9,11 +9,11 @@ ActionMailer::Base.smtp_settings = {
     :port => '587',
     :authentication => :plain,
     #:user_name => ENV['SMTP_RELAY_USERNAME'],
-    :user_name => ENV['GMAIL_USERNAME'],
-    #:user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['GMAIL_PASSWORD'],
+    #:user_name => ENV['GMAIL_USERNAME'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    #:password => ENV['GMAIL_PASSWORD'],
     #:password => ENV['SMTP_RELAY_PASSWORD'],
-    #:password => ENV['SENDGRID_PASSWORD'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
