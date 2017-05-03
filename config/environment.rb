9d7a2862749d433b68f6718473fd7531a792e7ac
Alpha Blog <princybathla999@yahoo.com>
2017-05-03 15:07:23 -0400

    Email functionality working

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '465',
+    :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yahoo.com',
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
