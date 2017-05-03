3be015caeb9332629733bf6efbe15dcb2a5f27e0
Alpha Blog <princybathla999@yahoo.com>
2017-05-03 12:00:08 -0400

    SMTP RELAY ADDED

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
 gem 'certified'
+gem 'figaro'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,12 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['SMTP_RELAY_USERNAME'],
+    #:user_name => ENV['GMAIL_USERNAME'],
+    #:user_name => ENV['SENDGRID_USERNAME'],
+    #:password => ENV['GMAIL_PASSWORD'],
+    :password => ENV['SMTP_RELAY_PASSWORD'],
+    #:password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
