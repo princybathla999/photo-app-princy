0dab7f7f55653fad63cb272aa2d48318c996ddd6
Alpha Blog <princybathla999@yahoo.com>
2017-05-03 12:07:02 -0400

    smtp gmail

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,14 @@ require_relative 'application'
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
+    #:port => '465',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SMTP_RELAY_USERNAME'],
-    #:user_name => ENV['GMAIL_USERNAME'],
+    #:user_name => ENV['SMTP_RELAY_USERNAME'],
+    :user_name => ENV['GMAIL_USERNAME'],
     #:user_name => ENV['SENDGRID_USERNAME'],
-    #:password => ENV['GMAIL_PASSWORD'],
-    :password => ENV['SMTP_RELAY_PASSWORD'],
+    :password => ENV['GMAIL_PASSWORD'],
+    #:password => ENV['SMTP_RELAY_PASSWORD'],
     #:password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
